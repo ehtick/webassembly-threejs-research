c984f93f5071b8b7c23818115552347d816030f7
a23dilel <a23dilel@student.his.se>
2026-03-13 10:44:36 +0100

    Refactor the DebugGUI class and add some properties inside the params object

--- src/js/debugGUI.js
+++ src/js/debugGUI.js
@@ -9,40 +9,44 @@ class DebugGUI {
         this.deleteButton;
 
         this.PARAMS = {
-            // Performance
-            fps: 0,
-            calls: 0,
-            triangles: 0,
-            JsHeapMB: 0,
-            
-            // GPU Memory
-            geometries: 0,
-            textures: 0,
-            
-            // Particles Geometry
-            type: 'points',
-            count: 100,
-            size: 0.5,
-            color: 0x00ff00,
-            posBounds: 50,
-            speed: 5,
-            wireframe: false,
+            performance: {
+                fps: 0,
+                calls: 0,
+                triangles: 0,
+                JsHeapMB: 0,
+            },
 
-            // Presets
-            savePreset: () => {
-                saveToLocalStorage( 'preset', this.gui.save() );
-                this.loadButton.enable();
-                this.deleteButton.enable();
+            gpuMemory: {
+                geometries: 0,
+                textures: 0
             },
-            loadPreset: () => {
-                this.gui.load(loadFromLocalStorage( 'preset' ));
+            
+            particlesGeometry: {
+                type: 'points',
+                count: 100,
+                size: 0.5,
+                color: 0x00ff00,
+                posBounds: 50,
+                speed: 5,
+                wireframe: false
             },
-            deleteAllPresets: () => {
-                if (confirm( 'Do you want to delete all presets?' )) {
-                    removeLocalStorage( 'preset' );
-                    this.loadButton.disable();
-                    this.deleteButton.disable();
-                } 
+            
+            presets: {
+                savePreset: () => {
+                    saveToLocalStorage( 'preset', this.gui.save() );
+                    this.loadButton.enable();
+                    this.deleteButton.enable();
+                },
+                loadPreset: () => {
+                    this.gui.load(loadFromLocalStorage( 'preset' ));
+                },
+                deleteAllPresets: () => {
+                    if (confirm( 'Do you want to delete all presets?' )) {
+                        removeLocalStorage( 'preset' );
+                        this.loadButton.disable();
+                        this.deleteButton.disable();
+                    } 
+                }
             }
         }
     }
@@ -51,59 +55,48 @@ class DebugGUI {
         let { gui } = this;
         const { PARAMS } = this;
 
-        // Controller properties
+        for (let propertyName in PARAMS) {            
+            const folder = gui.addFolder( propertyName );
 
-        const PERF_FOLDER = gui.addFolder( 'Performance' );
-        PERF_FOLDER.add( PARAMS, 'fps' ).listen();
-        PERF_FOLDER.add( PARAMS, 'calls' ).listen();
-        PERF_FOLDER.add( PARAMS, 'triangles' ).listen();
-        PERF_FOLDER.add( PARAMS, 'JsHeapMB').listen();
-        
-        const GPU_MEMORY_FOLDER = gui.addFolder( 'GPU Memory' );
-        GPU_MEMORY_FOLDER.add( PARAMS, 'geometries' ).listen().name( 'Geometries' );
-        GPU_MEMORY_FOLDER.add( PARAMS, 'textures' ).listen().name( 'Textures' );
-        
-        const PARTICLES_GEOMETRY_FOLDER = gui.addFolder( 'Particles Geometry' );
-        PARTICLES_GEOMETRY_FOLDER.add( PARAMS, 'type', [ 'points', 'cubes' ] ).name( 'Type' ).onChange(() => {
-            createGeometry(PARAMS);
-        });
+            if (propertyName == "particlesGeometry") {
+                for (let key in PARAMS[propertyName]) {
+                    const capitalize = key.charAt(0).toLocaleUpperCase() + key.substring(1);
+                    
+                    if (key == "type") {
+                        folder.add( PARAMS[propertyName], key, [ 'points', 'cubes' ] ).name(capitalize).onChange(() => {
+                            createGeometry(PARAMS[propertyName]);
+                        });
+                    } else if (key == "color") {
+                        folder.addColor( PARAMS[propertyName], key ).name(capitalize).onChange(() => {
+                            createGeometry(PARAMS[propertyName]);
+                        });
+                    } else {
+                        folder.add( PARAMS[propertyName], key ).name(capitalize).onChange(() => {
+                            createGeometry(PARAMS[propertyName]);
+                        });
+                    }
+                }
+            } else if (propertyName == "presets") {
+                folder.add( PARAMS[propertyName], 'savePreset' ).name( 'Save preset' );
+                this.loadButton = folder.add( PARAMS[propertyName], 'loadPreset' ).name( 'Load preset' );
+                this.deleteButton = folder.add( PARAMS[propertyName], 'deleteAllPresets' ).name( 'Delete all presets' );
 
-        PARTICLES_GEOMETRY_FOLDER.add( PARAMS, 'count' ).name( 'Count' ).onChange(() => {
-            createGeometry(PARAMS);
-        });
+                let preset = loadFromLocalStorage( 'preset' )
 
-        PARTICLES_GEOMETRY_FOLDER.add( PARAMS, 'size' ).name( 'Size' ).onChange(() => {
-            createGeometry(PARAMS);
-        });
-
-        PARTICLES_GEOMETRY_FOLDER.addColor( PARAMS, 'color' ).name( 'Color' ).onChange(() => {
-            createGeometry(PARAMS);
-        });
-
-        PARTICLES_GEOMETRY_FOLDER.add( PARAMS, 'posBounds' ).name( 'Position Bounds' ).onChange(() => {
-            createGeometry(PARAMS);
-        });
-
-        PARTICLES_GEOMETRY_FOLDER.add( PARAMS, 'speed' ).name( 'Speed' ).onChange(() => {
-            createGeometry(PARAMS);
-        });
-        
-        PARTICLES_GEOMETRY_FOLDER.add( PARAMS, 'wireframe' ).name( 'Wireframe' );
-        
-        const PRESETS_FOLDER = gui.addFolder( 'Presets' );
-        PRESETS_FOLDER.add( PARAMS, 'savePreset' ).name( 'Save preset' );
-        this.loadButton = PRESETS_FOLDER.add( PARAMS, 'loadPreset' ).name( 'Load preset' );
-        this.deleteButton = PRESETS_FOLDER.add( PARAMS, 'deleteAllPresets' ).name( 'Delete all presets' );
-        
-        let preset = loadFromLocalStorage( 'preset' )
-
-        if (preset != null) {
-            this.loadButton.enable();
-            this.deleteButton.enable();
-            gui.load(preset);
-        } else {
-            this.loadButton.disable();
-            this.deleteButton.disable();
+                if (preset != null) {
+                    this.loadButton.enable();
+                    this.deleteButton.enable();
+                    gui.load(preset);
+                } else {
+                    this.loadButton.disable();
+                    this.deleteButton.disable();
+                }
+            } else {
+                for (let key in PARAMS[propertyName]) {
+                    const capitalize = key.charAt(0).toLocaleUpperCase() + key.substring(1);
+                    folder.add( PARAMS[propertyName], key ).listen().name(capitalize);
+                }
+            }
         }
     }
 }
