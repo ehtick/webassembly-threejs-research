e288a35e21f3981d0384759c50ee19616fb28c26
a23dilel <a23dilel@student.his.se>
2026-03-23 15:05:33 +0100

    Refactor index.js code and replace setGeometry() with removeScene() and addScene()

--- src/js/index.js
+++ src/js/index.js
@@ -8,32 +8,36 @@ const container = document.body;
 
 if (WebGL.isWebGL2Available()) {  
   const debugGUI = new DebugGUI({container: container.canvas});
+  const { type, count, spread, speed, size, color, wireframe, bounceable: isBounceable} = debugGUI.object.particles.input;
+  const { running: isRunning } = debugGUI.object.threeApp.input;
+  
+  let particles = new Particles({ type: type.default, count, spread, speed, size, color, wireframe, isBounceable });
+  
   const fpsCounter = new FPSCounter();
-  let particles = new Particles();
-
-  if(debugGUI) {
-    const { type, count, posBounds, size, color, speed, wireframe } = debugGUI.object.particlesGeometry.input;
-    particles = new Particles({ type: type.default, count, posBounds, size, color, speed, wireframe});
-  } 
-
   const threeApp = new ThreeApp({debugGUI, fpsCounter});
+
   threeApp.createRenderer({container, width: window.innerWidth, height: window.innerHeight});
   threeApp.createCamera({aspect: window.innerWidth / window.innerHeight, enableControls: true});
-  threeApp.setGeometry(particles);
-  threeApp.setRunning(true);
-  
+  threeApp.addScene(particles);
+  threeApp.setRunning(isRunning);
+
   window.addEventListener("resize", (event) => {
     threeApp.updateRenderer({width: window.innerWidth, height: window.innerHeight});
     threeApp.updateCamera({aspect: window.innerWidth / window.innerHeight});
   })
 
-  debugGUI.start({onChange: update})
-  
   function update(object) {
-    const { type, count, posBounds, size, color, speed, wireframe } = object.particlesGeometry.input;
-    particles = new Particles({ type: type.default, count, posBounds, size, color, speed, wireframe});
-    threeApp.setGeometry(particles);
+    const { type, count, spread, speed, size, color, wireframe, bounceable: isBounceable } = object.particles.input;
+    const { running: isRunning } = object.threeApp.input;
+
+    threeApp.removeScene();
+    particles.updateSetup({ type: type.default, count, spread, speed, size, color, wireframe, isBounceable });
+    threeApp.addScene(particles);
+    
+    threeApp.setRunning(isRunning);
   }
+
+  debugGUI.start({onChange: update});
 } else {
   const warning = WebGL.getWebGL2ErrorMessage();
   container.appendChild(warning);
