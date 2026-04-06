75ca11479e0bd75a524d9ed849fb190c8c3c9a1f
a23dilel <a23dilel@student.his.se>
2026-04-06 13:09:31 +0200

    Add release flag in batch and shell scripts, and add optimize code cargo rust

--- scripts/compile_rust_to_wasm.bat
+++ scripts/compile_rust_to_wasm.bat
@@ -5,7 +5,8 @@ echo Make a build/rust directory in the project root...
 mkdir build/rust
 
 echo Compiling Rust to WebAssembly...
-wasm-pack build --target web --out-dir build/rust --out-name lib --no-pack --no-typescript
+:: release flag will call cargo file in [profile.release]
+wasm-pack build --target web --out-dir build/rust --out-name lib --no-pack --no-typescript --release
 
 :: Remove .gitignore in the build/rust directory
 del build\rust\.gitignore
--- scripts/compile_rust_to_wasm.sh
+++ scripts/compile_rust_to_wasm.sh
@@ -7,7 +7,8 @@ echo "Make a build/rust directory in the project root..."
 mkdir -p build/rust
 
 echo "Compiling Rust to WebAssembly..."
-wasm-pack build --target web --out-dir build/rust --out-name lib --no-pack --no-typescript
+# release flag will call cargo file in [profile.release]
+wasm-pack build --target web --out-dir build/rust --out-name lib --no-pack --no-typescript --release
 
 # Remove .gitignore in the build/rust directory
 rm build/rust/.gitignore
