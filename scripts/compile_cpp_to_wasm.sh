5d31bf99f1f029a19a20d4d610be59118e70e147
a23dilel <a23dilel@student.his.se>
2026-04-06 13:02:16 +0200

    Add optimize code c++ and export _malloc, _free and HEAP32 in batch and shell scripts

--- scripts/compile_cpp_to_wasm.sh
+++ scripts/compile_cpp_to_wasm.sh
@@ -10,6 +10,6 @@ echo "Entering the C++ directory..."
 cd src/c++
 
 echo "Compiling C++ to WebAssembly..."
-em++ -std=c++17 lib.cpp -s ENVIRONMENT=web -s MODULARIZE -s EXPORT_ES6 -lembind -o ../../build/c++/lib.js
+em++ -std=c++17 -Os -flto lib.cpp -s ENVIRONMENT=web -s MODULARIZE -s EXPORT_ES6 -s EXPORTED_FUNCTIONS="['_malloc','_free']" -s EXPORTED_RUNTIME_METHODS="['HEAPF32']" -lembind -o ../../build/c++/lib.js
 
 echo "Done! Successfully compiled C++ to WebAssembly in the build/c++ directory."
